e9caf338384836bf16d06e1de22dfc1048d58104
Aboudy Kreidieh <akreidieh@gmail.com>
2022-09-21 11:27:56 -0700

    removed from init

--- il_traffic/experts/__init__.py
+++ il_traffic/experts/__init__.py
@@ -1,10 +1,8 @@
 """Init file for the il_traffic/experts folder."""
 from il_traffic.experts.fs import PISaturation
 from il_traffic.experts.idm import IntelligentDriverModel
-from il_traffic.experts.non_local import NonLocalHarmonizer
 
 __all__ = [
     "PISaturation",
     "IntelligentDriverModel",
-    "NonLocalHarmonizer",
 ]
